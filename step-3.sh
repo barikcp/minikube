ca4a41ff5faf16872f83330c838e71d1181e7953
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-31 00:57:27 +0530

    Update step-3.sh

--- step-3.sh
+++ step-3.sh
@@ -2,5 +2,9 @@
 
 #Install Minikube
 
-sudo minikube start --driver=docker --force       # Install Minikube
-kubectl get node                  #check the node status
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64   # Download the package
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
+minikube version
+sudo minikube start --driver=docker --force   # Install Minikube
+kubectl get node          # Check the status of the node
